b487aa94d63612735fed678cefba123ba10c1074
Ling930616 <lingling930616@gmail.com>
2024-05-22 15:26:53 +0800

    v1_朱芷伶

--- app/src/main/java/tw/edu/pu/csim/s1113422/s1113422/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/s1113422/s1113422/MainActivity.kt
@@ -4,12 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import tw.edu.pu.csim.s1113422.s1113422.ui.theme.S1113422Theme
 
@@ -32,11 +36,15 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+    Image(
+        painter = painterResource(id = R.drawable.moyi),
+        contentDescription = "圖片",
+        alpha = 0.7f,
+        modifier = Modifier
+            .background(Color.Black)
     )
 }
+
 
 @Preview(showBackground = true)
 @Composable
